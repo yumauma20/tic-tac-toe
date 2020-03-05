000c7540956506a7007a60b05de0a8aa0dfd532d
yumauma20 <71yuma10@gmail.com>
2020-03-05 14:52:17 +0900

    勝敗判定ロジック編

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // flagがfalseのときバツのターン、trueのときマルのターン
 let flag = false;
 
+let counter = 9;
+
 // square を取得
 const squares = document.querySelectorAll('.square');
 // Array に変換　※IE11対策
@@ -22,6 +24,40 @@ function setMessage(id) {
   }); 
 }
 
+// タテ・ヨコ・ナナメの列を配列化
+function filterById(targetArray, idArray) {
+  return targetArray.filter(function(e) {
+    return (e.id === idArray[0] || e.id === idArray[1] || e.id === idArray[2]);
+  });
+}
+
+const line1 = filterById(squaresArray, ['1-1', '1-2', '1-3']);
+const line2 = filterById(squaresArray, ['2-1', '2-2', '2-3']);
+const line3 = filterById(squaresArray, ['3-1', '3-2', '3-3']);
+const line4 = filterById(squaresArray, ['1-1', '2-1', '3-1']);
+const line5 = filterById(squaresArray, ['1-2', '2-2', '3-2']);
+const line6 = filterById(squaresArray, ['1-3', '2-3', '3-3']);
+const line7 = filterById(squaresArray, ['1-1', '2-2', '3-3']);
+const line8 = filterById(squaresArray, ['1-3', '2-2', '3-1']);
+
+const lineArray = [line1, line2, line3, line4, line5, line6, line7, line8];
+
+// 勝敗判定
+function isWinner(symbol) { 
+  const result = lineArray.some(function (line) {
+    const subResult = line.every(function (square) {
+      if (symbol === 'maru') {
+        return square.classList.contains('js-maru-checked');
+      } else 
+      if (symbol === 'batsu') {
+        return square.classList.contains('js-batsu-checked');
+      }
+    });
+    return subResult;
+  });
+  return result;
+}
+
 // マス目をクリックしたときにイベント発火
 squaresArray.forEach(function (square) {
   square.addEventListener('click', function () {
@@ -29,14 +65,41 @@ squaresArray.forEach(function (square) {
     if (flag === true) {
       square.classList.add('js-maru-checked');
       square.classList.add('js-unclickable');
+      
+      // マル勝利判定
+      if (isWinner('maru')) {
+        setMessage('maru-win');
+        return;
+      }
+      
       setMessage('batsu-turn');
       flag = false;
 
     } else {
       square.classList.add('js-batsu-checked');
       square.classList.add('js-unclickable');
+
+      // バツ勝利判定
+      if (isWinner('batsu')) {
+        setMessage('batsu-win');
+        return;
+      }
+
       setMessage('maru-turn');
       flag = true;
     }
+
+    if (flag === true) {
+
+    }else{
+
+    }
+
+    counter--;
+
+    if (counter === 0) {
+      setMessage('draw');
+    }
+
   });
 });
